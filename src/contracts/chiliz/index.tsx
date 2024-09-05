24073d74ffd52c56d3d3eb9b6eff01ca80f107a0
Vib-UX <vibhav@instaraise.io>
2024-09-05 21:33:15 +0530

    contracts: joinTeam fanbattle

--- src/contracts/chiliz/index.tsx
+++ src/contracts/chiliz/index.tsx
@@ -1,143 +1,143 @@
-import { FAN_BATTLE_ABI } from '@/abi/FAN_BATTLE_ABI';
-import { FAN_TOKEN_ABI } from '@/abi/FAN_TOKEN_ABI';
-import { M2E_FITNESS_ABI } from '@/abi/M2E_FITNESS_ABI';
+import { FAN_BATTLE_ABI } from "@/abi/FAN_BATTLE_ABI";
+import { FAN_TOKEN_ABI } from "@/abi/FAN_TOKEN_ABI";
+import { M2E_FITNESS_ABI } from "@/abi/M2E_FITNESS_ABI";
 
 import {
-    FAN_BATTLE_CONTRACT_ADDRESS,
-    FAN_TOKEN_CONTRACT_ADDRESS,
-    MOVE_TO_EARN_CONTRACT_ADDRESS_CHILIZ,
-} from '@/config/addresses';
-import { ethers } from 'ethers';
-import { encodeFunctionData } from 'viem';
+  FAN_BATTLE_CONTRACT_ADDRESS,
+  FAN_TOKEN_CONTRACT_ADDRESS,
+  MOVE_TO_EARN_CONTRACT_ADDRESS_CHILIZ,
+} from "@/config/addresses";
+import { ethers } from "ethers";
+import { encodeFunctionData } from "viem";
 
 export async function recordWorkoutWithSigner(
-    signer: string,
-    providerUrl: string
+  signer: string,
+  providerUrl: string
 ) {
-    console.log(signer);
+  console.log(signer);
 
-    try {
-        const moveToEarnAddress = MOVE_TO_EARN_CONTRACT_ADDRESS_CHILIZ;
+  try {
+    const moveToEarnAddress = MOVE_TO_EARN_CONTRACT_ADDRESS_CHILIZ;
 
-        // Create the encoded transaction data
-        const workoutTx = encodeFunctionData({
-            abi: M2E_FITNESS_ABI,
-            functionName: 'recordDailyWorkout',
-            args: [],
-        });
-        console.log(workoutTx);
+    // Create the encoded transaction data
+    const workoutTx = encodeFunctionData({
+      abi: M2E_FITNESS_ABI,
+      functionName: "recordDailyWorkout",
+      args: [],
+    });
+    console.log(workoutTx);
 
-        const workoutTransaction = {
-            to: moveToEarnAddress,
-            data: workoutTx,
-        };
-        console.log(`Workout Transaction Data: ${workoutTransaction.data}`);
+    const workoutTransaction = {
+      to: moveToEarnAddress,
+      data: workoutTx,
+    };
+    console.log(`Workout Transaction Data: ${workoutTransaction.data}`);
 
-        // Initialize a provider and a signer using the private key
-        const provider = new ethers.JsonRpcProvider(providerUrl);
-        const wallet = new ethers.Wallet(signer, provider);
+    // Initialize a provider and a signer using the private key
+    const provider = new ethers.JsonRpcProvider(providerUrl);
+    const wallet = new ethers.Wallet(signer, provider);
 
-        // Send the transaction using the wallet
-        console.log('Sending transaction through private key signer...');
-        const transactionResponse = await wallet.sendTransaction(
-            workoutTransaction
-        );
-        console.log('Transaction sent, waiting for confirmation...');
+    // Send the transaction using the wallet
+    console.log("Sending transaction through private key signer...");
+    const transactionResponse = await wallet.sendTransaction(
+      workoutTransaction
+    );
+    console.log("Transaction sent, waiting for confirmation...");
 
-        const receipt = await transactionResponse.wait();
-        console.log('Transaction receipt:', receipt);
+    const receipt = await transactionResponse.wait();
+    console.log("Transaction receipt:", receipt);
 
-        return receipt;
-    } catch (error) {
-        console.error('Error in recordWorkoutWithPrivateKey', error);
-    }
+    return receipt;
+  } catch (error) {
+    console.error("Error in recordWorkoutWithPrivateKey", error);
+  }
 }
 export async function joinTeamWithSigner(
-    signer: ethers.Signer,
-    teamName: string
+  signer: string,
+  providerUrl: string,
+  teamName: string
 ) {
-    console.log(signer);
-    console.log('Starting joinTeamWithSigner');
+  try {
+    const fanBattleAddress = FAN_BATTLE_CONTRACT_ADDRESS;
 
-    try {
-        const fanBattleAddress = FAN_BATTLE_CONTRACT_ADDRESS;
+    // Create the encoded transaction data for joinTeam
+    const joinTeamTx = encodeFunctionData({
+      abi: FAN_BATTLE_ABI,
+      functionName: "joinTeam",
+      args: [teamName],
+    });
+    console.log(joinTeamTx);
 
-        // Create the encoded transaction data for joinTeam
-        const joinTeamTx = encodeFunctionData({
-            abi: FAN_BATTLE_ABI,
-            functionName: 'joinTeam',
-            args: [teamName],
-        });
-        console.log(joinTeamTx);
+    const joinTeamTransaction = {
+      to: fanBattleAddress,
+      data: joinTeamTx,
+    };
+    console.log(`Join Team Transaction Data: ${joinTeamTransaction.data}`);
 
-        const joinTeamTransaction = {
-            to: fanBattleAddress,
-            data: joinTeamTx,
-        };
-        console.log(`Join Team Transaction Data: ${joinTeamTransaction.data}`);
+    // Initialize a provider and wallet using the private key
+    const provider = new ethers.JsonRpcProvider(providerUrl);
+    const wallet = new ethers.Wallet(signer, provider);
 
-        // Send the transaction using the signer
-        console.log('Sending joinTeam transaction through signer...');
-        const transactionResponse = await signer.sendTransaction(
-            joinTeamTransaction
-        );
-        console.log('Transaction sent, waiting for confirmation...');
+    // Send the transaction using the wallet
+    console.log("Sending joinTeam transaction through private key signer...");
+    const transactionResponse = await wallet.sendTransaction(
+      joinTeamTransaction
+    );
+    console.log("Transaction sent, waiting for confirmation...");
 
-        const receipt = await transactionResponse.wait();
-        console.log('Transaction receipt:', receipt);
+    const receipt = await transactionResponse.wait();
+    console.log("Transaction receipt:", receipt);
 
-        return receipt;
-    } catch (error) {
-        console.error('Error in joinTeamWithSigner', error);
-    }
+    return receipt;
+  } catch (error) {
+    console.error("Error in joinTeamWithSigner", error);
+  }
 }
 
 export async function getFanTokenBalance(userAddress: string | null) {
-    console.log(`Getting FanToken balance for user: ${userAddress}`);
+  console.log(`Getting FanToken balance for user: ${userAddress}`);
 
-    try {
-        const provider = new ethers.JsonRpcProvider(
-            'https://spicy-rpc.chiliz.com/'
-        ); // chiliz rpc
+  try {
+    const provider = new ethers.JsonRpcProvider(
+      "https://spicy-rpc.chiliz.com/"
+    ); // chiliz rpc
 
-        // Create an instance of the FanToken contract
-        const fanTokenContract = new ethers.Contract(
-            FAN_TOKEN_CONTRACT_ADDRESS,
-            FAN_TOKEN_ABI,
-            provider
-        );
+    // Create an instance of the FanToken contract
+    const fanTokenContract = new ethers.Contract(
+      FAN_TOKEN_CONTRACT_ADDRESS,
+      FAN_TOKEN_ABI,
+      provider
+    );
 
-        const balance = await fanTokenContract.balanceOf(userAddress);
-        console.log(`FanToken Balance: ${balance.toString()}`);
+    const balance = await fanTokenContract.balanceOf(userAddress);
+    console.log(`FanToken Balance: ${balance.toString()}`);
 
-        return balance;
-    } catch (error) {
-        console.error('Error in getFanTokenBalance', error);
-    }
+    return balance;
+  } catch (error) {
+    console.error("Error in getFanTokenBalance", error);
+  }
 }
 
 export async function sendTestTokensChiliz(address: string | null) {
-    const provider = new ethers.JsonRpcProvider(
-        'https://spicy-rpc.chiliz.com/'
-    );
-    console.log(`Getting test funds for address: ${address}`);
+  const provider = new ethers.JsonRpcProvider("https://spicy-rpc.chiliz.com/");
+  console.log(`Getting test funds for address: ${address}`);
 
-    // Setup a third-party provider with its own signer to send transactions
-    const thirdPartyProvider = new ethers.Wallet(
-        process.env.NEXT_PUBLIC_P_KEY || '',
-        provider
-    );
-    console.log('Third party provider:', thirdPartyProvider);
-    // Sending CHZ to the signer address
-    console.log('Sending CHZ to the signer address...');
-    const ethSendPromise = await thirdPartyProvider.sendTransaction({
-        to: address,
-        value: ethers.parseUnits('1', 18), // Sending 1 CHZ
-    });
-    await ethSendPromise.wait();
-    console.log(
-        `CHZ transfer successful, transaction hash: ${ethSendPromise.hash}`
-    );
+  // Setup a third-party provider with its own signer to send transactions
+  const thirdPartyProvider = new ethers.Wallet(
+    process.env.NEXT_PUBLIC_P_KEY || "",
+    provider
+  );
+  console.log("Third party provider:", thirdPartyProvider);
+  // Sending CHZ to the signer address
+  console.log("Sending CHZ to the signer address...");
+  const ethSendPromise = await thirdPartyProvider.sendTransaction({
+    to: address,
+    value: ethers.parseUnits("1", 18), // Sending 1 CHZ
+  });
+  await ethSendPromise.wait();
+  console.log(
+    `CHZ transfer successful, transaction hash: ${ethSendPromise.hash}`
+  );
 
-    return { trxhash: ethSendPromise.hash };
+  return { trxhash: ethSendPromise.hash };
 }
