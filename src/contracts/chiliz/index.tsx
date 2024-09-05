310b6366cdd059e6a5432abe2f6b525305fc1ba5
Vib-UX <vibhav@instaraise.io>
2024-09-05 16:28:21 +0530

    contracts: p2p fan-battle chiliz

--- src/contracts/chiliz/index.tsx
+++ src/contracts/chiliz/index.tsx
@@ -2,6 +2,10 @@ import { ethers } from "ethers";
 import { M2E_FITNESS_ABI } from "@/abi/M2E_FITNESS_ABI";
 import { MOVE_TO_EARN_CONTRACT_ADDRESS_CHILIZ } from "@/config/addresses";
 import { encodeFunctionData } from "viem";
+import { FAN_BATTLE_ABI } from "@/abi/FAN_BATTLE_ABI";
+import { FAN_BATTLE_CONTRACT_ADDRESS } from "@/config/addresses";
+import { FAN_TOKEN_ABI } from "@/abi/FAN_TOKEN_ABI";
+import { FAN_TOKEN_CONTRACT_ADDRESS } from "@/config/addresses";
 
 export async function recordWorkoutWithSigner(signer: ethers.Signer) {
   console.log(signer);
@@ -37,5 +41,69 @@ export async function recordWorkoutWithSigner(signer: ethers.Signer) {
     return receipt;
   } catch (error) {
     console.error("Error in recordWorkoutWithSigner", error);
+  }
+}
+
+export async function joinTeamWithSigner(
+  signer: ethers.Signer,
+  teamName: string
+) {
+  console.log(signer);
+  console.log("Starting joinTeamWithSigner");
+
+  try {
+    const fanBattleAddress = FAN_BATTLE_CONTRACT_ADDRESS;
+
+    // Create the encoded transaction data for joinTeam
+    const joinTeamTx = encodeFunctionData({
+      abi: FAN_BATTLE_ABI,
+      functionName: "joinTeam",
+      args: [teamName],
+    });
+    console.log(joinTeamTx);
+
+    const joinTeamTransaction = {
+      to: fanBattleAddress,
+      data: joinTeamTx,
+    };
+    console.log(`Join Team Transaction Data: ${joinTeamTransaction.data}`);
+
+    // Send the transaction using the signer
+    console.log("Sending joinTeam transaction through signer...");
+    const transactionResponse = await signer.sendTransaction(
+      joinTeamTransaction
+    );
+    console.log("Transaction sent, waiting for confirmation...");
+
+    const receipt = await transactionResponse.wait();
+    console.log("Transaction receipt:", receipt);
+
+    return receipt;
+  } catch (error) {
+    console.error("Error in joinTeamWithSigner", error);
+  }
+}
+
+export async function getFanTokenBalance(userAddress: string) {
+  console.log(`Getting FanToken balance for user: ${userAddress}`);
+
+  try {
+    const provider = new ethers.JsonRpcProvider(
+      "https://spicy-rpc.chiliz.com/"
+    ); // chiliz rpc
+
+    // Create an instance of the FanToken contract
+    const fanTokenContract = new ethers.Contract(
+      FAN_TOKEN_CONTRACT_ADDRESS,
+      FAN_TOKEN_ABI,
+      provider
+    );
+
+    const balance = await fanTokenContract.balanceOf(userAddress);
+    console.log(`FanToken Balance: ${balance.toString()}`);
+
+    return balance;
+  } catch (error) {
+    console.error("Error in getFanTokenBalance", error);
   }
 }
