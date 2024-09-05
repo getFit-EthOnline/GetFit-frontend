7c3c379dd9c2da40154dcfaf0af36ed955ed535d
KetanKudikyal <krotusppp@gmail.com>
2024-09-05 13:47:04 +0530

    fix: 🐛 remove comments

--- src/app/bundledFrame/page.tsx
+++ src/app/bundledFrame/page.tsx
@@ -2,19 +2,13 @@ import { getFrameMetadata } from "@coinbase/onchainkit/frame";
 import { Metadata } from "next";
 
 const frameMetadata = getFrameMetadata({
-    // Accepts and isOpenFrame keys are required for Open Frame compatibility
     accepts: { xmtp: "2024-02-09" },
     isOpenFrame: true,
     buttons: [
         {
-            // Whatever label you want your first button to have
             label: "Make batch transaction",
-            // Required 'tx' action for a transaction frame
             action: "tx",
-            // Below buttons are 2 route urls that will be added in the next steps.
-            // Target will send back info about the transaction
             target: `${process.env.NEXT_PUBLIC_BASE_URL}/api/bundled-transaction`,
-            // postUrl will send back a transaction success screen
             postUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/api/transaction-success`,
         },
     ],
